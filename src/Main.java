a6e313aaaf7df0a0d08340c6b12accf9afab1f37
Andrei Pronsky <andrey.pronsky@gmail.com>
2022-10-24 15:25:12 +0300

    Hometask completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        User user = new User("Andrei", "Pronsky", "andrey.pronsky@mail.com", "1234", 1);
+        UserDataAccess.create(user);
+        UserDataAccess.update(user);
+        System.out.println(UserDataAccess.find(5L));
+        List<User> users = UserDataAccess.findAll();
+        for (User u : users) {
+            System.out.println(u);
+        }
+        System.out.println(UserDataAccess.deleteById(6L));
+        UserDataAccess.printTableInfo();
     }
 }
