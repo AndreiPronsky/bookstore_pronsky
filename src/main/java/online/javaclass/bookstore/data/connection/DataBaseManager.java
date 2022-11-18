8e1a3dcd39868190756787c95d84d6745ef3f2fe
Andrei Pronsky <andrey.pronsky@gmail.com>
2022-11-18 09:38:57 +0300

    Refatored DBManager, removed ConnectionProperties as redundant

--- src/main/java/online/javaclass/bookstore/data/connection/DataBaseManager.java
+++ src/main/java/online/javaclass/bookstore/data/connection/DataBaseManager.java
@@ -3,20 +3,30 @@ package online.javaclass.bookstore.data.connection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
-import java.util.Map;
+import java.util.Properties;
 
 public class DataBaseManager implements AutoCloseable {
     private final ConnectionPool pool;
+    final String PATH_TO_PROPS = "/connection-config.properties";
+    final String url;
+    final String user;
+    final String password;
     private static final Logger log = LogManager.getLogger();
 
     public DataBaseManager() {
-        ConnectionProperties properties = new ConnectionProperties();
-        String PATH_TO_PROPS = "/connection-config.properties";
-        final Map<String, String> props = properties.getConnectionProperties(PATH_TO_PROPS);
-        String url = props.get("URL");
-        String user = props.get("USER");
-        String password = props.get("PASSWORD");
+        Properties properties = new Properties();
+        try (InputStream input = this.getClass().getResourceAsStream(PATH_TO_PROPS)) {
+            properties.load(input);
+        } catch (IOException e) {
+            log.error("unable to extract connection properties");
+        }
+
+        url = properties.getProperty("db.url");
+        user = properties.getProperty("db.user");
+        password = properties.getProperty("db.password");
         pool = new ConnectionPool(url, user, password);
     }
 
