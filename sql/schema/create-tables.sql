735b6fd05fe29f9ccb88b8a5e258940bff335034
Andrei Pronsky <andrey.pronsky@gmail.com>
2022-11-23 16:40:24 +0300

    added tables for genres, roles, covers. pom.xml modified

--- sql/schema/create-tables.sql
+++ sql/schema/create-tables.sql
@@ -1,10 +1,25 @@
+CREATE TABLE roles (
+    id BIGSERIAL PRIMARY KEY,
+    role_name VARCHAR(32)
+);
+
+CREATE TABLE covers (
+    id BIGSERIAL PRIMARY KEY,
+    cover_name VARCHAR(32)
+);
+
+CREATE TABLE genres (
+    id BIGSERIAL PRIMARY KEY,
+    genre_name VARCHAR(32)
+);
+
 CREATE TABLE users (
     user_id BIGSERIAL PRIMARY KEY,
     firstname VARCHAR(128),
     lastname VARCHAR(128),
     email VARCHAR(128) UNIQUE NOT NULL,
     user_password VARCHAR(128) NOT NULL,
-    user_role VARCHAR(128),
+    role_id BIGINT REFERENCES roles,
     rating NUMERIC(3, 2)
     );
 
@@ -13,7 +28,8 @@ CREATE TABLE books (
     title VARCHAR(128) NOT NULL,
     author VARCHAR(128) NOT NULL,
     isbn VARCHAR(128) UNIQUE NOT NULL,
-    cover VARCHAR(128),
+    genre BIGINT REFERENCES genres,
+    cover BIGINT REFERENCES covers,
     pages INTEGER,
     price NUMERIC(5, 2) NOT NULL,
     rating NUMERIC(3, 2)
