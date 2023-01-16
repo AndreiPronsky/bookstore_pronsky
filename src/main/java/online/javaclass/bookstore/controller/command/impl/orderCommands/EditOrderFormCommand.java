13e17b4c6189b818d9ea4a7db88ae2b709e5c017
Andrei Pronsky <andrey.pronsky@gmail.com>
2023-01-16 15:02:13 +0300

    fixed edit order vulnerability

--- src/main/java/online/javaclass/bookstore/controller/command/impl/orderCommands/EditOrderFormCommand.java
+++ src/main/java/online/javaclass/bookstore/controller/command/impl/orderCommands/EditOrderFormCommand.java
@@ -4,12 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import online.javaclass.bookstore.controller.FrontController;
 import online.javaclass.bookstore.controller.command.Command;
 import online.javaclass.bookstore.service.BookService;
 import online.javaclass.bookstore.service.OrderService;
 import online.javaclass.bookstore.service.dto.BookDto;
 import online.javaclass.bookstore.service.dto.OrderDto;
 import online.javaclass.bookstore.service.dto.OrderItemDto;
+import online.javaclass.bookstore.service.dto.UserDto;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,8 +24,12 @@ public class EditOrderFormCommand implements Command {
 
     @Override
     public String execute(HttpServletRequest req) {
-        Long orderId = Long.parseLong(req.getParameter("id"));
         HttpSession session = req.getSession();
+        if (session.getAttribute("user") == null ||
+                !((UserDto)session.getAttribute("user")).getRole().equals(UserDto.Role.ADMIN)) {
+            return FrontController.REDIRECT + "index.jsp";
+        }
+        Long orderId = Long.parseLong(req.getParameter("id"));
         OrderDto order = orderService.getById(orderId);
         Map<BookDto, Integer> itemMap = new HashMap<>();
         for (OrderItemDto item : order.getItems()) {
