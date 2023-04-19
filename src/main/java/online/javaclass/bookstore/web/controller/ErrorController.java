0f3ec088592d5e542b992bc6ee445ccce947acb2
Andrei Pronsky <andrey.pronsky@gmail.com>
2023-04-19 20:52:25 +0300

    Mapstruct integrated, add_book.jsp spring migration completed. User entity and dto refactored due to userPreferences failures

--- src/main/java/online/javaclass/bookstore/web/controller/ErrorController.java
+++ src/main/java/online/javaclass/bookstore/web/controller/ErrorController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.ServletException;
+
 @Controller
 @RequiredArgsConstructor
 @ControllerAdvice(annotations = Controller.class)
@@ -50,7 +52,7 @@ public class ErrorController {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handle(Model model, RuntimeException e) {
+    public String handle(Model model, RuntimeException e, ServletException ex) {
         model.addAttribute("message",
                 messageSource.getMessage("error.default_server", new Object[]{}, LocaleContextHolder.getLocale()));
         return "error";
